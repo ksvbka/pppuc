624c68d88304459ceba26efe645081b94239797b
ksvbka <letrungkien.k53.hut@gmail.com>
2016-05-07 18:50:21 +0700

    Fixed some issue, add error code

--- chap6/grammar.cpp
+++ chap6/grammar.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
+#include <limits>
 
 using namespace std;
+
+enum error_code{
+    ER_NOUN,
+    ER_VERB,
+    ER_CONJ,
+    NO_ERR
+};
 
 bool is_noun(string token){
     return( token == "birds" || token == "fish" || token == "c++");
@@ -14,37 +22,51 @@ bool is_conjunction(string token){
     return( token == "and" || token == "or" || token == "but");
 }
 
-string sentence(){
+int sentence(){
     string token;
     cin >> token;
     if(!is_noun(token)){
-        // cout << "\nFalse - expected noun" << endl;
-        return "false";
+        // return "nFalse - expected noun";
+        return ER_NOUN;
     }
     cin >> token;
     if(!is_verb(token)){
-        // cout << "\nFalse - expected verb" << endl;
-        return "false";
+        // return "False - expected verb";
+        return ER_VERB;
     }
     if(cin >>token){
         if(is_conjunction(token))
-        sentence();
+            sentence();
+        else
+            return ER_CONJ;
     }
-    return "ok";
+    return NO_ERR;
 }
 
-int main(int argc, char const *argv[])
-{
+int main(int argc, char const *argv[]){
+
     cout << "sentence demo:" << endl;
     string token;
-    bool val = false;
     while(cin){
         char ch;
         cin >> ch;
         if(ch == 'q') break;
         cin.putback(ch);
 
-        cout << sentence() << endl;
+        int ret = sentence();
+        switch(ret){
+            case ER_NOUN:
+                cout << "False - expected noun" << endl; break;
+            case ER_VERB:
+                cout << "False - expected verb" << endl; break;
+            case ER_CONJ:
+                cout << "False - expected conjunction" << endl; break;
+            case NO_ERR:
+                cout << "ok" << endl; break;
+        }
+
+        cin.clear();
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
     }
     return 0;
 }
